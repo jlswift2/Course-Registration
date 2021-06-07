ea086aa0e76a472247aa676fd5206d19a3e90171
Justin Swift <jlswift2@gmail.com>
2021-06-08 00:04:32 +0900

    My Course Registration app, second commit

--- course/urls.py
+++ course/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('course/', views.course_search, name='course_search'),
     path('course/<slug:course_number>/', views.course_desc, name='course_desc'),
-    path('course/<)
-
 ]
--- course/views.py
+++ course/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 import json
 from .models import Course,Prof,Location
 
-# def course_desc(request, course_number):
-#     course = Course.objects.get(number=course_number)
-#     return render(request, 'Course_Search/Course_Desc.html', {'course': course})
-#
-# def course_list(request, course_name):
-#     courses = Course.objects.filter(name__icontains=course_name)
-#     return render(request, 'Course_Search/Course_List.html', courses)
+
+def home(request):
+    return render(request, 'Home_Page/Home_Page.html', {})
+
+
+def course_desc(request, course_number):
+    course = Course.objects.get(number=course_number)
+    return render(request, 'Course_Search/Course_Desc.html', {'course': course})
+
+
+def course_search(request):
+    data = {}
+    if request.method == 'GET':
+        courses = Course.objects.filter(name__icontains='course_name')
+        data = {'courses': courses}
+
+    return render(request, 'Course_Search/Course_Search.html', data)
