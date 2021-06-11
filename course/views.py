90db2d9d8e5809b48643b739192a5df03197865d
jlswift2 <jlswift2@gmail.com>
2021-06-12 00:44:19 +0900

    Updates to Course Search

--- course/views.py
+++ course/views.py
@@ -14,8 +14,12 @@ def course_desc(request, course_number):
 
 def course_search(request):
     data = {}
-    if request.method == 'GET':
-        courses = Course.objects.filter(name__icontains='course_name')
-        data = {'courses': courses}
-
+    really = False
+    print("request;", request.GET)
+    if request.method == 'GET' and 'name' in request.GET.keys():
+        really = True
+        courses = Course.objects.filter(name__icontains=request.GET.get('name'))
+        data['courses'] = courses
+    data['really'] = really
+    print(data)
     return render(request, 'Course_Search/Course_Search.html', data)
