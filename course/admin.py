6fd7a1613ed6a871bd35da31974498f18d73c40c
jlswift2 <jlswift2@gmail.com>
2021-07-26 22:26:14 +0900

    register student model

--- course/admin.py
+++ course/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Course,Prof, Enrollment
+from .models import Course, Prof, Enrollment, Student
 
+admin.site.register(Student)
 admin.site.register(Course)
 admin.site.register(Prof)
 admin.site.register(Enrollment)
