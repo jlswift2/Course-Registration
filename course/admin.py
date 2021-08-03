37f2c7b2dc896611ab08b66315ac99d28cbd3f8a
jlswift2 <jlswift2@gmail.com>
2021-08-03 22:55:36 +0900

    register new Department model

--- course/admin.py
+++ course/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Course, Enrollment, Student, Professor
+from .models import Course, Enrollment, Student, Professor, Department
 
 admin.site.register(Student)
 admin.site.register(Professor)
+admin.site.register(Department)
 admin.site.register(Course)
 admin.site.register(Enrollment)
