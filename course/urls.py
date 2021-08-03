0456d38f50d39a6694f24f8c706701e09bf8eb52
jlswift2 <jlswift2@gmail.com>
2021-08-03 21:59:20 +0900

    add course browse url

--- course/urls.py
+++ course/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('', views.home, name='home'),
     path('course/', views.course_search, name='course_search'),
+    path('course/browse', views.course_browse, name='course_browse'),
     path('course/<int:pk>/', views.course_desc, name='course_desc'),
     path('prof/<int:pk>/', views.prof_bio, name='prof_bio'),
     path('user/<slug:user.username>/', views.account_dashboard, name='account_dashboard')
