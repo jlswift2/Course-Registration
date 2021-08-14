0272353893f3299c5839ead85a17f3b0ee689850
jlswift2 <jlswift2@gmail.com>
2021-08-14 19:16:02 +0900

    create url for unenrollment confirmation

--- course/urls.py
+++ course/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('prof/<int:pk>/', views.prof_bio, name='prof_bio'),
     path('user/dashboard/', views.account_dashboard, name='account_dashboard'),
     path('user/schedule', views.my_classes, name='schedule'),
+    path('user/schedule/unenroll/<str:pk>/', views.unenroll, name='unenroll')
 ]
